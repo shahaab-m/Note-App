25091433b0971a403f6db221f75cac83be15dffe
Shahaab Manzar <shahaab.m@getphyllo.com>
2024-02-08 03:10:58 +0530

    CRUD operations done, getting saved to postgres using postman

--- src/main.py
+++ src/main.py
@@ -1,29 +1,64 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-
+from database import SessionLocal
+import models
 app = FastAPI()
+db = SessionLocal()
 
+class NoteBaseModal(BaseModel):
+    class Config:
+        orm_mode = True
 
-class Note(BaseModel):
+class Note(NoteBaseModal):
     id: int
     author: str
     content: str
 
+@app.get('/', response_model=list[Note], status_code=status.HTTP_200_OK)
+def getAllNotes():
+    allPosts = db.query(models.Note).all()
+    return allPosts
 
-@app.get('/', status_code=200)
-def getNoteInfo():
-    return {"message": "server is running"}
+@app.post('/create_note', response_model=Note, status_code=status.HTTP_201_CREATED)
+def createNote(note:Note):
+    newNote = models.Note(
+        id = note.id,
+        author = note.author,
+        content = note.content
+    )
 
+    findNote = db.query(models.Note).filter(models.Note.id == note.id).first()
 
-@app.get('/getNoteById/{note_id}', status_code=200)
-def getNoteById(note_id: int):
-    return {"message": f"Your Note Id is {note_id}"}
+    if findNote is not None:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Note with this id already exist")
 
+    db.add(newNote)
+    db.commit()
 
-@app.post('/addNote', status_code=200)
-def addNote(note: Note):
-    return {
-        "id": note.id,
-        "author": note.author,
-        "content": note.content
-    }
+    return newNote
+
+@app.put('/update_note/{noteId}', response_model=Note, status_code=status.HTTP_202_ACCEPTED)
+def updateNote(noteId:int, note:Note):
+    findNote = db.query(models.Note).filter(models.Note.id == note.id).first()
+    findNote.id = note.id,
+    findNote.author = note.author,
+    findNote.content = note.content
+
+    if findNote is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note with this id doesn't exist")
+
+    db.commit()
+
+    return findNote
+
+@app.delete('/delete_note/{noteId}', response_model=Note, status_code=status.HTTP_200_OK)
+def deleteNote(noteId:int, note:Note):
+    findNoteToDelete = db.query(models.Note).filter(models.Note.id == note.id).first()
+    db.delete(findNoteToDelete)
+
+    if findNoteToDelete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note with this id doesn't exist")
+
+    db.commit()
+
+    return findNoteToDelete
